e2550cbd668ca000f59e0ee59b647e45a96e8145
Faizuddin Safa <safarajuk19@gmail.com>
2026-02-05 21:57:43 +0600

    Char and string input

---
+++ CharMethods.java
@@ -0,0 +1,12 @@
+public class CharMethods {
+    public static void main(String[] args) {
+        String name = "abc, def";
+        if()
+        System.out.println(name.charAt(3));
+        String out = "Java".toUpperCase();
+        System.out.println(out);
+        char c1 = name.charAt(0);
+        System.out.println(c1);
+
+    }
+}
