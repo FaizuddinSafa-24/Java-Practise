ccd0cd8b80be961c7c1793ac2219eaa7b540fa36
Faizuddin Safa <safarajuk19@gmail.com>
2026-02-08 12:00:27 +0600

    Output from array book checking

---
+++ OutputCheck.java
@@ -0,0 +1,9 @@
+public class OutputCheck {
+    public static void main(String[] args) {
+        int x = 30;
+        int[] numbers = new int[x];
+        x =30;
+        System.out.println("x is "+ x);
+        System.out.println("The size of numbers is "+ numbers.length);
+    }
+}
