5e372b7c8914cef186fbb013c236aa5b62e886f5
Faizuddin Safa <safarajuk19@gmail.com>
2026-02-09 12:37:57 +0600

    Adding scanner using java.util.Scanner direct in main and arraycopy with new method

--- CopyArray.java
+++ CopyArray.java
@@ -1,7 +1,11 @@
 import java.util.Arrays;
+import java.util.Scanner;
 public class CopyArray {
     public static void main(String[] args) {
         int[] arr = {3,4,5,6,7,8,9};
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+        System.out.println(num);
         int[] copy = new int[arr.length];
         System.arraycopy(arr, 0, copy, 0, arr.length);
         System.out.println("The copied arrays :"+Arrays.toString(copy));
