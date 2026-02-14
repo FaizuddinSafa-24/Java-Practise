890bfd9c01c8aae1ac794fd2547ed93b204ffec8
Faizuddin Safa <safarajuk19@gmail.com>
2026-02-14 09:14:29 +0600

    Boundary sum fixed. Previously, I included only top corners

--- Arr2DBoundarySum.java
+++ Arr2DBoundarySum.java
@@ -2,40 +2,35 @@ public class Arr2DBoundarySum {
     public static void main (String[] args) {
         // array initialize
         int[][] arr = new int[4][4];
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0 ; j < 4 ; j++) {
+        for (int i = 0; i <arr.length; i++) {
+            for (int j = 0 ; j <arr.length ; j++) {
                 arr[i][j] = (int) (Math.random()*10);
             }
         }
         // test print
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0 ; j < 4 ; j++) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0 ; j <arr.length ; j++) {
                 System.out.print(arr[i][j]);
             }
             System.out.println();
         }
-        System.out.println();
+        
         int sum = 0;
-        // sum
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0 ; j < 4 ; j++) {
-                if (i == 0 && j == 0) {
+        int i = 0 ,j = 0;
+        for (i = 0; i < arr.length; i++) {
+            for (j = 0 ; j <arr.length; j++) {
+                if (i == 0 || i == arr.length - 1 || j == 0|| j == arr.length - 1) {
+
                     sum += arr[i][j];
-                } else if (i == 0 && j == 3) {
-                    sum += arr[i][j];
-                    
-                } else if (i == 3 && j == 0) {
-                    
-                    sum += arr[i][j];
-                }else if (i == 3 && j == 3) {
-                    
-                    sum += arr[i][j];
-                } 
+                }
             }
+            System.out.println();
         }
+        // sum
+       
         // final print
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0 ; j < 4 ; j++) {
+        for (i = 0; i <arr.length; i++) {
+            for (j = 0 ; j <arr.length ; j++) {
                 System.out.print(arr[i][j]);
             }
             System.out.println();
--- Arr2DIEqualJ.java
+++ Arr2DIEqualJ.java
@@ -1,19 +1,9 @@
 public class Arr2DIEqualJ {
     public static void main(String[] ags) {
         int[][] matrix = new int[4][4];
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                matrix[i][j] = 0;
-            }
-        }
-        // print to check
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                System.out.print(matrix[i][j]);
-            }
-            System.out.println();
-        }
-        System.out.println();
+        
+        // arrays in java auto initialized
+       
         // i == j
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
@@ -30,8 +20,5 @@ public class Arr2DIEqualJ {
             }
             System.out.println();
         }
-        
-        
-
     }
 }
