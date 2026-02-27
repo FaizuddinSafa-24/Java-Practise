f740738de6a6611049d535a6a95fd79c261c5f39
Faizuddin Safa <safarajuk19@gmail.com>
2026-02-27 15:22:35 +0600

    numerous bug fixed in many files

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,1 +1,4 @@
-num
+long second = timeISeconds / 1000;
+        // long minute = (long) second / 60;
+        // long hr = minute / 60;
+        // System.out.printf("miliseconds: %f, seconds = %f, minutes= %d, hours = %d", timeISeconds, second, minute, hr);
