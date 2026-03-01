09e6319f04fcdf081c20f39a71bbf510d2075d93
Faizuddin Safa <safarajuk19@gmail.com>
2026-03-01 16:22:31 +0600

    Inheritance and encapsulation together

---
+++ Car.java
@@ -0,0 +1,42 @@
+class Model { // Inherited class from Car
+
+    // Think inhertitance as receive and send
+
+    // inherited= receive
+    
+    // being inherited = send/ received to
+    
+    protected String carName; 
+    // encapsulated and have access in the same package and subclasses
+    // N.B: class "Model" is a subclass of "Car" super-class
+    private int carAge = 25;
+
+    // private ensures that this data is only accessible inside here.
+    //N.B: by getter method it can be accessed from another class file(super/sub)
+
+    public int getCarAge() {
+        return carAge;
+    }
+
+    
+    public String getCarName() {
+        return carName;
+    }
+    public void setCarAge(int carAge) {
+        this.carAge = carAge;
+    }
+
+    public void  honk() {
+        System.out.println("pip pip!");
+    }
+}
+
+class Car extends Model { // this is the super class that has sub-class access
+    public static void main(String[] args) {
+        Car newCar = new Car();
+        newCar.carName = "Toyota Axio";
+        System.out.println(newCar.getCarName());
+        newCar.setCarAge(30);
+        System.out.println(newCar.getCarAge());
+    }
+}
