51b148b271a8363b89a990cc074fbfee4d89bacd
Faizuddin Safa <safarajuk19@gmail.com>
2026-03-09 15:19:34 +0600

    Circle1 project created with custom dependencies

---
+++ Circle1/CircleStatic/src/Circle.java
@@ -0,0 +1,17 @@
+public class Circle {
+    double radius;
+    static int numOfObj;
+    
+    Circle() {
+        radius = 1;
+        numOfObj++;
+    }
+    Circle(double newRadius) {
+        this.radius = newRadius;
+        numOfObj++;
+    }
+    static int getNumOfObj() {
+        return numOfObj;
+    }
+    
+}
---
+++ Circle1/CircleStatic/src/TestCircle.java
@@ -0,0 +1,8 @@
+public class TestCircle {
+    public static void main(String[] args) {
+        System.out.println(Circle.numOfObj);
+        Circle c1 = new Circle();
+        System.out.println(Circle.getNumOfObj());
+        System.out.println(Math.PI);
+    }
+}
