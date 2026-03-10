fc4b10aafb5c449def91704c7fd538d4b958928a
Faizuddin Safa <safarajuk19@gmail.com>
2026-03-10 12:57:12 +0600

    closed scanner and check printf formatting '%-30s%-15f\n'

--- AlphaCheck.java
+++ AlphaCheck.java
@@ -5,12 +5,15 @@ public class AlphaCheck {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter a character from English alphabet: ");
         alpha = scan.next().charAt(0);
+        scan.close();
         System.out.println(alpha);
         if (alpha == 'a' || alpha == 'e' || alpha == 'i' || alpha == 'o' || alpha == 'u') {
             System.out.println("You entered a vowel.");
         } else {
             System.out.println("You entered a consonant.");
         }
+        double record = 156.36;
+        System.out.printf("%-30s%-15f\n","Total area of circle is ",record);
             
     }
 }
