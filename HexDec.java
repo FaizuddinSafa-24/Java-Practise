e8f44ee5deec2a76f05b5f852486b95af7bcdee7
Faizuddin Safa <safarajuk19@gmail.com>
2026-03-12 08:38:34 +0600

    add limit of the hex to 16 inside 'parseint'

--- HexDec.java
+++ HexDec.java
@@ -4,10 +4,10 @@ public class HexDec {
     public static void main(String[] args) {
         System.out.println("Enter a hexadecimal to convert Binary: ");
         Scanner scan = new Scanner(System.in);
-        String input = scan.nextLine();
-        int decimal = Integer.parseInt(input);
+        String input = scan.next(); // no nextLine(), only next()
+        int decimal = Integer.parseInt(input,16); // define regex(regular expression) to 16
         String binary = Integer.toBinaryString(decimal);
         System.out.printf("The binary of %s is %s\n", input, binary);
     }
-    
+
 }
