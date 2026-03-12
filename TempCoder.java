061d19fba1e49300919e47c9e09e59417df9dd13
Faizuddin Safa <safarajuk19@gmail.com>
2026-03-12 08:23:45 +0600

    Time calling fixed

---
+++ TempCoder.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+public class TempCoder {
+        public static void main(String[] args) {
+                Date date = new Date();
+                System.out.println(date.toString());
+                //long second = timeISeconds / 1000;
+                        // long minute = (long) second / 60;
+                        // long hr = minute / 60;
+                        // System.out.printf("miliseconds: %f, seconds = %f, minutes= %d, hours = %d", timeISeconds, second, minute, hr);
+        }
+}
+
--- tempCodeRunnerFile.java
+++
@@ -1,4 +0,0 @@
-long second = timeISeconds / 1000;
-        // long minute = (long) second / 60;
-        // long hr = minute / 60;
-        // System.out.printf("miliseconds: %f, seconds = %f, minutes= %d, hours = %d", timeISeconds, second, minute, hr);
