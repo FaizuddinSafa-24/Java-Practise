050da4f1e81d26b055d4c8291f77c3683da78fb5
Faizuddin Safa <safarajuk19@gmail.com>
2026-03-12 08:25:21 +0600

    Wrong statements deleted

--- TempCoder.java
+++ TempCoder.java
@@ -4,10 +4,7 @@ public class TempCoder {
         public static void main(String[] args) {
                 Date date = new Date();
                 System.out.println(date.toString());
-                //long second = timeISeconds / 1000;
-                        // long minute = (long) second / 60;
-                        // long hr = minute / 60;
-                        // System.out.printf("miliseconds: %f, seconds = %f, minutes= %d, hours = %d", timeISeconds, second, minute, hr);
+                // invoking Date obj to  string
         }
 }
 
